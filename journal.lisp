cadff3a71276bb5e73065ae799dc4a1c84c32d40
Matthias Benkard <code@mail.matthias.benkard.de>
2007-05-31 11:28:35 +0200

    Apply the Affero GPL.
    darcs-hash:2a8ada315969000b38a1abd6b61780a8cf448f56

--- journal.lisp
+++ journal.lisp
@@ -2,6 +2,25 @@
 ;;;; -*- coding: utf-8; mode: lisp -*-
 ;;;; Copyright 2007, Matthias Andreas Benkard.
 
+;;;-----------------------------------------------------------------------
+;;; This file is part of The Mulkblog Project.
+;;;
+;;; The Mulkblog Project is free software.  You can redistribute it and/or
+;;; modify it under the terms of the Affero General Public License as
+;;; published by Affero, Inc.; either version 1 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; The Mulkblog Project is distributed in the hope that it will be
+;;; useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+;;; of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; Affero General Public License for more details.
+;;;
+;;; You should have received a copy of the Affero General Public
+;;; License in the COPYING file that comes with The Mulkblog Project; if
+;;; not, write to Affero, Inc., 510 Third Street, Suite 225, San
+;;; Francisco, CA 94107 USA.
+;;;-----------------------------------------------------------------------
+
 
 ;;; TAKE NOTICE: If you want to run this script from the command line or
 ;;; from a web server, be sure to use a core image including the systems
