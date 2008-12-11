b455cae0cd4336448e787d6cc5d61ee1943677cc
Matthias Benkard <code@mail.matthias.benkard.de>
2008-12-11 21:06:54 +0100

    Do not try to set CFFI:*DEFAULT-FOREIGN-ENCODING*.
    darcs-hash:d5007595e2b8041ecb37df737581c09618d78fe6

--- main.lisp
+++ main.lisp
@@ -191,7 +191,7 @@
   (dolist (env-var '("HTTP_CACHE_CONTROL" "HTTP_IF_MODIFIED_SINCE"))
     (pushnew env-var http::*http-env-vars*))
   (http:http-init)
-  (setq cffi:*default-foreign-encoding* :iso-8859-15)
+  #+mst-plus (setq cffi:*default-foreign-encoding* :iso-8859-15)
   (handler-bind
       ((error #'
         (lambda (e)
