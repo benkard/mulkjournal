e8a3524a2c1c6ed3d7385a0c31825a3385d6bbf6
Matthias Benkard <code@mail.matthias.benkard.de>
2009-10-07 12:18:24 +0200

    Do not depend on the presence of the Common-Lisp Controller.
    Ignore-this: 83781260a8ffe3318af46a4e53bdf38
    
    darcs-hash:b21a8ab0bc0c9160043e6f2d83452b5421d2e520

--- make-core-image.sh
+++ make-core-image.sh
@@ -3,7 +3,7 @@ clisp -q -q -on-error exit <<EOF
 (dolist (system '(:cl-ppcre :cl-fad :iterate :cl-markdown :parenscript
                   :yaclml :lisp-cgi-utils :alexandria :xml-emitter
                   :split-sequence :clsql :clsql-sqlite3 :drakma))
-  (clc:clc-require system))
+  (asdf:oos 'asdf:load-op system))
 (saveinitmem "lispinit.mem")
 (quit)
 EOF
