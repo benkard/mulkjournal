eaad370b048bcb11f087fdf97528bfb31141ad0e
Matthias Benkard <code@mail.matthias.benkard.de>
2009-10-23 23:36:43 +0200

    Add a width specification to the viewport meta tag.
    Ignore-this: 4597ab8cf4540b50a6c2b78b59d75d9e
    
    darcs-hash:2c6b24e148cadf8417ba2f02ddcd7c64e609ee3f

--- journal.lisp
+++ journal.lisp
@@ -438,7 +438,7 @@
     ;; designed to work with small devices.  It's not so great when your
     ;; pages mostly consist of lots of text to read, though.  Therefore,
     ;; let's disable the default scaling here.
-    (<:meta :name "viewport" :content "initial-scale=1.0")
+    (<:meta :name "viewport" :content "initial-scale=1.0, width=device-width")
     (<:title
      (<:as-is
       (if page-title
