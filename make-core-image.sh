15937a1a4f1cf40bc55aa34eb71c67b88466ff57
Matthias Benkard <code@mail.matthias.benkard.de>
2009-10-30 20:45:40 +0100

    Update build scripts.
    Ignore-this: d92417cd0e6528a1be2808f7060d91a6
    
    darcs-hash:1d70dd932f38b6b8a9a590c1717c7f4d433c5361

--- make-core-image.sh
+++ make-core-image.sh
@@ -2,8 +2,11 @@
 clisp -q -q -on-error exit <<EOF
 (dolist (system '(:cl-ppcre :cl-fad :iterate :cl-markdown :parenscript
                   :yaclml :lisp-cgi-utils :alexandria :xml-emitter
-                  :split-sequence :clsql :clsql-sqlite3 :drakma :cybertyggr-time))
-  (asdf:oos 'asdf:load-op system))
+                  :split-sequence :clsql :clsql-sqlite3 :drakma :cybertyggr-time
+                  :s-xml-rpc))
+  (handler-bind
+      ((error (lambda (&rest args) (invoke-restart (find-restart 'continue)))))
+    (asdf:oos 'asdf:load-op system)))
 (saveinitmem "lispinit.mem")
 (quit)
 EOF
